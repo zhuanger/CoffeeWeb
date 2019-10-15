6077804ad62a169f2b514606abe0293a79cacfad
unknown <984145377@qq.com>
2019-10-15 23:02:52 +0800

    添加时间戳/自动递增

--- models/userModel.js
+++ models/userModel.js
@@ -1,14 +1,17 @@
 const Sequelize = require('sequelize');
 const User = Sequelize.define('user', {
   name: {
-    type: STRING,
+    type: Sequelize.STRING,
     unique: true, allowNull: false
   },
   userId: {
     primaryKey: true,
     allowNull: false,
-    type: STRING,
-    autoIncrement:true,
-  }
+    type: Sequelize.INTEGER,
+    autoIncrement:true, //首先在数据库那里创建一条数据 id为1000
+  },
+  // 时间戳
+  createdAt: Sequelize.DATE,
+  updatedAt: Sequelize.DATE,
 })
 module.exports = User
