a146de63625365dc76c736ce82a6ab50ad5a332b
pompill <908902377@qq.com>
2019-10-19 16:53:11 +0800

    改了返回字段

--- routes/admin/user/user.js
+++ routes/admin/user/user.js
@@ -1,13 +1,16 @@
 let userService = require(__services + 'userService')
 module.exports = function (app) {
   $ajax.get('/').then((res)=>{
+    console.log(res.query)
     userService.getUserInfo().then((result)=>{
       console.log('step three', result)
       //统一返回格式
       res.response.send({
         code: 200,
         msg: '查询成功',
-        data: result
+        data: {
+          result: result
+        }
       })
       //记得最后加next() https://stackoverflow.com/questions/51535455/express-js-use-async-function-on-requests/51538169#51538169
       res.next() 
